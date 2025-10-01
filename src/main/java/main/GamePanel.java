0ddf1388fe0bd7b847e7b7cc7f1e840fd0712296
Hubert Kozłowski <business.kozlowskihubert@gmail.com>
2025-10-01 21:07:09 +0100

    init

---
+++ src/main/java/main/GamePanel.java
@@ -0,0 +1,48 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12; // 16:9 aspect ratio
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); // for better rendering performance
+
+
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+
+            System.out.println("The game is running");
+
+            // 1 update: update information such as character positions
+            // 2 draw: draw the screen with the updated information
+
+
+        }
+    }
+}
